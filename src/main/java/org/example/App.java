3c59ac26cad3925570d39a96fffeec86d3858bb6
John <catalin_chira05@yahoo.com>
2023-05-29 12:22:50 +0300

    Am creat un bouncing ball

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,15 @@
+package org.example;
+
+
+public class App 
+{
+
+    public static void main( String[] args )
+    {
+
+        Bouncer bouncer= new Bouncer();
+
+        bouncer.init();
+
+    }
+}
